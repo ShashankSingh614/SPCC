7f31fca98abe5e04f93ef26cbb36723f922185c7
Shashank Singh <Singhshashankthakur596@gmail.com>
2025-02-10 11:48:57 +0530

    Update codeOptimization.py

--- codeOptimization.py
+++ codeOptimization.py
@@ -7,24 +7,27 @@ def optimize_code(code):
         var = var.strip()
         expr = expr.strip()
 
-        if expr in subexpressions:
+        if expr in subexpressions and var not in expr:
+            optimized_code.append(f"{var} = {subexpressions[expr]}") 
+        elif expr in subexpressions and var in expr:
             optimized_code.append(f"{var} = {subexpressions[expr]}")
-            del subexpressions[expr]
+            del subexpressions[expr]  
         else:
             subexpressions[expr] = var
-            optimized_code.append(line)
+            optimized_code.append(line) 
     return optimized_code
 
 code_before_optimization = [
-    "x = y + z",
-    "a = y + z",
-    "b = x + y",
-    "c = x + y",
-    "d = y + z"
+    "a = b + c",
+    "d = b + c",
+    "c = b + c",
 ]
 
 optimized_code = optimize_code(code_before_optimization)
-
+print("Code before optimization:")
+for l1 in code_before_optimization:
+    print(l1)
+print()
 print("Optimized code:")
-for line in optimized_code:
-    print(line)
+for l2 in optimized_code:
+    print(l2)
